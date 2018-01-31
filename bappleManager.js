af62836da8b97c87b4b5167e213954d5912abf09
Kist221 <Kist221@gmail.com>
2018-01-30 20:03:44 -0700

    added manger functions to list, low inventory, and add stock

--- bappleManager.js
+++ bappleManager.js
@@ -30,7 +30,7 @@ function managerMenu() {
     	} else if (answers.choice === "View Low Inventory") {
     		lowInventory();
     	} else if (answers.choice === "Add Inventory") {
-    		
+    		productChoice();
     	} else if (answers.choice === "Add New Product") {
     		
     	} else if (answers.choice === "EXIT") {
@@ -41,27 +41,19 @@ function managerMenu() {
   });
 };
 
-managerMenu();
-
 // function to list all products
 function listProducts() {
   var query = "SELECT * FROM products";
   connection.query(query, function (error, res, fields) {
     if (error) throw error;
-    // if items exist
-    if (res.length !== 0) {
-      console.log("\nChecking Stock...")
-      for (var i = 0; i < res.length; i++) {
-        console.log("\nID: " + res[i].item_id + " | " + res[i].product_name + " | Price: $" + res[i].price + " | Stock: " + res[i].stock);
-      }
-      console.log();
-    } else {
-      console.log("\nSorry. Nothing is currently in stock. Please check back soon!");
-    }
+	console.log("\nChecking Stock...");
+	for (var i = 0; i < res.length; i++) {
+	  console.log("\nID: " + res[i].item_id + " | " + res[i].product_name + " | Price: $" + res[i].price + " | Stock: " + res[i].stock);
+	}
+	console.log();
     managerMenu();
   });
 };
-
 
 // function to list all low inventory (stock) products
 function lowInventory() {
@@ -78,8 +70,85 @@ function lowInventory() {
       }
       console.log();
     } else {
+      console.log("\nChecking Stock...");
       console.log("\nAll Stock Above Minimum Threshhold!\n");
     }
     managerMenu();
   });
 };
+
+// function asks for itemID choice
+function productChoice() {
+  inquirer.prompt([
+     {
+       type: "input",
+       message: "Enter ID of product to add inventory:",
+       name: "productID"
+     }
+    ]).then(answers => {
+      findItem(answers.productID);
+  });
+};
+
+// function confirms and prints itemID choice
+function findItem(id) {
+  var query = "SELECT * FROM products WHERE item_id = ?";
+  connection.query(query, [id], function (error, res, fields) {
+    if (error) throw error;
+    if (res.length !== 0) {
+      console.log("\nsearching...")
+      console.log("\nID: " + res[0].item_id + " | " + res[0].product_name + " | Stock: " + res[0].stock + "\n");
+      quantityChoice(id);
+    } else {
+      console.log("\nsearching...")
+      console.log("\nProduct does not exist. Enter valid product ID.");
+      productChoice();
+    }
+  });
+};
+
+// function asks for quantity and checks against stock
+function quantityChoice(id) {
+  inquirer.prompt([
+     {
+       type: "input",
+       message: "Add quantity of:",
+       name: "quantity"
+     }
+    ]).then(answers => {
+      var query = "SELECT * FROM products WHERE item_id = ?";
+      connection.query(query, [id], function (error, res, fields) {
+        if (error) throw error;
+        var stock = res[0].stock;
+        var amount = answers.quantity;
+        if (amount < 1 || amount % 1 !== 0) {
+          console.log("\nPlease input valid quantity.\n");
+          quantityChoice(id);
+        } else {
+          console.log("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+          console.log("Stock Added!");
+          console.log("ID: " + res[0].item_id + " | " + res[0].product_name);
+          console.log("Previous: " + stock)
+          console.log("Added: +" + amount);
+          console.log("Current Inventory: " + (+stock + +amount));
+          console.log("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+          var add = +stock + +amount;
+          stockAdd(id, add);
+        }
+      });
+  });
+};
+
+// function to add quantity to stock
+function stockAdd(id, add) {
+  var query = "UPDATE products SET ? WHERE ?";
+  connection.query(query, [{stock: add}, {item_id: id}], function (error, res, fields) {
+    if (error) throw error;
+    managerMenu();
+  });
+};
+
+
+
+// invoke the MANAGER PROGRAM!!!
+managerMenu();
